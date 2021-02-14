d4e9cffb5be7ab4fcd1f5cd61efd294de00424a2
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-02-14 16:05:19 +0530

    removed div

--- app/transcript_storage.py
+++ app/transcript_storage.py
@@ -2,6 +2,7 @@ import pymongo
 import json
 import requests
 import os
+from pydub import AudioSegment
 
 YOUR_API_KEY = os.environ['AZURE_KEY']
 client = pymongo.MongoClient(os.environ['MONGODB_URI'])
@@ -15,7 +16,7 @@ def transcript(YOUR_AUDIO_FILE):
 
 def get_text(audio):
     # Request that the Bing Speech API convert the audio to text
-    url = 'https://eastus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-IN'
+    url = 'https://eastus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US'
     headers = {
         'Ocp-Apim-Subscription-Key': YOUR_API_KEY,
         'Content-type': 'audio/wav',
@@ -37,6 +38,7 @@ def stream_audio_file(speech_file, chunk_size=1024):
 
 def insert_transcript(course, id, text, time):
     posts = db.transcript
+    print(text)
     data = {
         'course': course,
         'vid_id': id,
@@ -46,7 +48,17 @@ def insert_transcript(course, id, text, time):
     posts.insert_one(data)
 
 
+def create_file(filename, start_time):
+    t1 = start_time * 1000
+    t2 = (start_time + 18) * 1000
+    newAudio = AudioSegment.from_wav(filename)
+    newAudio = newAudio[t1:t2]
+    newAudio.export('app/audio.wav', format="wav")
+
+
 if __name__ == '__main__':
     # db.transcript.create_index([("transcript", pymongo.TEXT)])
-    print(db.transcript.find_one({"$text": {"$search": "complement"}}))
-    # insert_transcript(course='ip', id='1T_oAY8OP4aHcozPRYaejCp6k8UIWwH7z', text=transcript('app/audio.wav'), time='693')
+    # print(db.transcript.find_one({"$text": {"$search": "complement"}}))
+    time = 693 #860
+    # create_file(filename="app/orig.wav", start_time=time)
+    insert_transcript(course='ip', id='1T_oAY8OP4aHcozPRYaejCp6k8UIWwH7z', text=transcript('app/audio.wav'), time=time)
