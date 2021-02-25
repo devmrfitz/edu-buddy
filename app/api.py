8f83d82836340075dac7a129b1b3752d32ee6374
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-02-25 19:58:09 +0530

    Cleanup

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@ import flask
 from app.bot_brain import reply
 api = flask.Blueprint('api', __name__)
 
-@api.route("/get", methods=['GET','POST'])
+@api.route("/get", methods=['GET', 'POST'])
 def api_get():
     if flask.request.method == 'GET':
         return flask.render_template("temp.html")
