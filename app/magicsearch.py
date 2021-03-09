6dc560069e4e570f671181ef48f750844473c003
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-03-09 16:17:30 +0530

    temp

--- app/magicsearch.py
+++ app/magicsearch.py
@@ -9,7 +9,7 @@ def magic_search(query: str):
     if "https://www.quora.com" in result or "https://quora.com" in result:
         print("Received magicURL "+result, flush=True)
         try:
-            return BeautifulSoup(urlopen(result).read(), features="html.parser").prettify().split('"@type": "Answer", "text": "')[1].split("upvoteCount")[0][:-4].replace("\\u2019", "'").replace("\\n", "\n")
+            return BeautifulSoup(urlopen(result).read(), features="html.parser").prettify().split('"text": "')[1].split("upvoteCount")[0][:-4].replace("\\u2019", "'").replace("\\n", "\n")
         except:
             return "The answer to this question is restricted to Area 51 employees (and possibly aliens)."
     else:
