09126d17ae20f1559f4799c2f5982c03d01a44e7
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-03-09 12:32:32 +0530

    temp

--- app/magicsearch.py
+++ app/magicsearch.py
@@ -7,10 +7,13 @@ def magic_search(query: str):
     for result in search(query + " site:quora.com", num=1, stop=1, pause=1):
         break
     if "https://www.quora.com" in result or "https://quora.com" in result:
-        return \
+        try:
+            return \
             BeautifulSoup(urlopen(result).read(), features="html.parser").prettify().split(
                 '"@type": "Answer", "text": "')[
                 1].split("upvoteCount")[0][:-4].replace("\\u2019", "'").replace("\\n", "\n")
+        except:
+            return "The answer to this question is restricted to Area 51 employees (and possibly aliens)."
     else:
         return "The answer to this question is restricted to Area 51 employees (and possibly aliens)."
 
